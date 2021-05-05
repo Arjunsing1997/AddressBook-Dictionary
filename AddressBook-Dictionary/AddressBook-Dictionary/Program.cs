f87a9f4448f44f6d3fb4f535d01272389b2e4fd6
Arjunsing1997 <acchur1997@gmail.com>
2021-05-05 21:08:48 +0530

    UC3 Editing Details

--- AddressBook-Dictionary/AddressBook-Dictionary/Program.cs
+++ AddressBook-Dictionary/AddressBook-Dictionary/Program.cs
@@ -55,16 +55,144 @@ namespace AddressBook_Dictionary
 
 
 
-            People.Add(person.City, person);
+            People.Add(person.FirstName, person);
+        }
+        private static void DIsplayPeople()
+        {
+            if (People.Count == 0)
+            {
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (KeyValuePair<string, Person> person in People)
+            {
+                PrintPerson(person.Value);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
+
+        private static void PrintPerson(Person person)
+        {
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Address : " + person.Address);
+            Console.WriteLine("City: " + person.City);
+            Console.WriteLine("Satet: " + person.State);
+            Console.WriteLine("ZIP: " + person.Zip);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Email :" + person.Email);
+
+            Console.WriteLine("-------------------------------------------");
+        }
+        public static void EditRecords()
+        {
+            Console.WriteLine("Enter first name:");
+            string fname = Console.ReadLine();
+
+            foreach (KeyValuePair<string, Person> item in People) //Value  present or not
+            {
+                if (item.Value.FirstName.Equals(fname))
+                {
+                    Person person = People[item.Value.FirstName];
+                    Console.WriteLine(person);  //Print person
+                    int choice = 0;
+                    while (choice != 7)  // k==0 to edite contact
+                    {
+                        Console.WriteLine("What Do You Want to edit Contact Details \n"
+                                + "1. Address\n"
+                                + "2. city\n"
+                                + "3. State\n"
+                                + "4. Zip Code\n"
+                                + "5. Phone\n"
+                                + "6. Email\n"
+                                + "7. Exit\n");
+
+                        choice = Convert.ToInt32(Console.ReadLine());  //convert string and store choice
+                        switch (choice)  //case 
+                        {
+                            case 1:
+                                Console.Write("Enter new Address:-  ");  //Take input user
+                                String address = Console.ReadLine();   //store address veriable
+                                item.Value.Address = address;  //store class of person address data
+                                break;
+                            case 2:
+                                Console.Write("Enter new City:- "); //Take input user
+                                String city = Console.ReadLine();  //store city veriable
+                                item.Value.City = city;                 //store class of person city data
+                                break;
+                            case 3:
+                                Console.Write("Enter new State:- "); //Take input user
+                                String state = Console.ReadLine();   //store state veriable
+                                item.Value.State = state;               //store class of person state data
+                                break;
+                            case 5:
+                                Console.Write("Enter new Phone:- "); //Take input user
+                                String phone = Console.ReadLine();   //store phone veriable
+                                item.Value.PhoneNumber = phone;                 //store class of person phone data
+                                break;
+                            case 4:
+                                Console.Write("Enter new Zip Code:- "); //Take input user
+                                String zip = Console.ReadLine();        //store zip veriable
+                                person.Zip = zip;                       //store class of person zip data
+                                break;
+                            case 6:
+                                Console.Write("Enter new Email:- "); //Take input user
+                                String email = Console.ReadLine();         //store email veriable
+                                item.Value.Email = email;                       //store class of person Email data
+                                break;
+                            case 7:
+                                break;
+                            default:
+                                Console.WriteLine("Please Enter Valid Option");
+                                break;
+                        }
+                        foreach (KeyValuePair<string, Person> t in People) //automate the reading  t of person of class
+                        {
+                            Console.WriteLine(t);//print list
+                        }
+                    }
+                }
+            }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook-Dictionary");
-            Console.WriteLine("Enter Add->To add details\n exit->To exit");
-            string command = Console.ReadLine();
+
+            string command = " ";
             while( command != "exit")
             {
-                AddPerson();
+                Console.Clear();
+                Console.WriteLine("Enter Commands\nAdd\nEdit\nDisplay\nexit");
+                Console.WriteLine("--------------------");
+                command = Console.ReadLine();
+
+                switch(command.ToLower())
+                {
+                    case "add":
+                        AddPerson();
+                        break;
+
+                    case "edit":
+                        EditRecords();
+                        break;
+
+                    case "display":
+                        DIsplayPeople();
+                        break;
+
+                    case "exit":
+                        Console.WriteLine("Exiting....");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Command");
+                        break;
+                }
+                
+
             }
             
 
